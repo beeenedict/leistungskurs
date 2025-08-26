9a0d0732b4bdab4803924545726c155ecda56608
beeenedict <benni.tailor@gmail.com>
2025-08-26 19:33:54 +0200

    Korrektur Zinssatz

--- Schule/src/blatt01/Zinssatz.java
+++ Schule/src/blatt01/Zinssatz.java
@@ -5,10 +5,12 @@ public class Zinssatz {
         double kapital = 1220.0;
         double zinssatz = 2.25;
         double zinsen = kapital * zinssatz / 100;
-        double n_kapital = kapital * zinssatz;
+        double n_kapital = kapital + zinsen;
+
         System.out.println("Altes Kapital: " + kapital + " €");
         System.out.println("Zinssatz: " + zinssatz + " %\n");
+        //kapital += zinsen;
         System.out.println("Zinsen: " + zinsen + " €");
-        System.out.println("Neues Kapital: " + n_kapital + " €");
+        System.out.println("Neues Kapital: " + n_kapital + " €"); // ... + kapital + ... (Startkapital nicht gespeichert)
     }
 }
