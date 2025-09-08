97f29a4e11226dda0f120b4005afee34dfa63fc8
beeenedict <benni.tailor@gmail.com>
2025-09-08 09:34:38 +0200

    Aufgabe 1 blatt04

---
+++ Schule/src/blatt04/ETF.java
@@ -0,0 +1,21 @@
+package blatt04;
+
+public class ETF {
+    public static void main(String[] args) {
+        double sparBetrag = 50;
+        double gesamtBetrag = 0;
+        double etfAnteil = 3578.89;
+        double anteil = 0;
+        int monat = 0;
+
+        while (monat < 12) {
+            monat ++;
+            gesamtBetrag += sparBetrag;
+            anteil = gesamtBetrag / etfAnteil;
+
+            System.out.println("Monat: " + monat);
+            System.out.printf("Gesamtbetrag: %.2f%n", gesamtBetrag);
+            System.out.printf("Anteil an einem ETF-Anteil: %.5f%n%n", anteil);
+        }
+    }
+}
