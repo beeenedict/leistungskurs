b78b51fa772127fbb44f30fcad5cd32c7940dda3
beeenedict <benni.tailor@gmail.com>
2025-09-17 11:19:13 +0200

    PasswortGruppeB.java angefangen

---
+++ Schule/src/blatt05/PasswortGruppeB.java
@@ -0,0 +1,18 @@
+package blatt05;
+
+import java.util.Scanner;
+
+public class PasswortGruppeB {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String passwort = input.nextLine();
+        while(true){
+            if (passwort.length() > 8){}
+            else {
+                break;
+            }
+        }
+
+    }
+}
