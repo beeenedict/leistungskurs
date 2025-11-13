2e496fc941b5dd9d7ed5737cc2bceae79114493c
beeenedict <benni.tailor@gmail.com>
2025-11-13 18:07:50 +0100

    blatt12 fertig

--- Schule/src/blatt12/SelectionSort.java
+++ Schule/src/blatt12/SelectionSort.java
@@ -3,21 +3,47 @@ package blatt12;
 public class SelectionSort {
 
     /**
-     * Sortiert die Elemente eines int-Arrays
+     * Sortiert die Elemente eines int-Arrays nach dem Max- oder MinSort.
      * @param arr int[]
+     * @param max boolean Max- oder MinSort
      */
 
-    public static void Selectionsort(int[] arr) {
-        int min;
+    public static void Selectionsort(int[] arr, boolean max) {
 
-        for (int i = 0; i < arr.length; i++) {
-            min = i;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
+        if (max) {
+            int maxi;
+
+            for (int i = arr.length - 1; i >= 0; i--) {
+                maxi = i;
+                for (int j = i; j >= 0; j--) {
+                    if (arr[j] > arr[maxi]) {
+                        maxi = j;
+                    }
                 }
+                blatt11.BubbleSort.swap(arr, maxi, i);
             }
-            blatt11.BubbleSort.swap(arr, min, i);
+        }
+
+        else {
+            int min;
+
+            for (int i = 0; i < arr.length; i++) {
+                min = i;
+                for (int j = i; j < arr.length; j++) {
+                    if (arr[j] < arr[min]) {
+                        min = j;
+                    }
+                }
+                blatt11.BubbleSort.swap(arr, min, i);
+            }
         }
     }
+
+    /*
+    public static void main(String[] args) {
+        int[] arr = new int[] { 100, 2, -245, 4, 5, 6, 7, -8, 9, 10 };
+        Selectionsort(arr, true);
+        blatt07.ArbeitMitArrays.printArray(arr);
+    }
+     */
 }
