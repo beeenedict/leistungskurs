b20343d383eda966d7fe7b0f7091c259e3be00b1
beeenedict <benni.tailor@gmail.com>
2025-12-02 13:02:29 +0100

    blatt13 PenAndPaper fertig

--- Schule/src/blatt13/PenAndPaper.java
+++ Schule/src/blatt13/PenAndPaper.java
@@ -5,7 +5,7 @@ public class PenAndPaper {
     public static int wuerfel(int N, int X) {
         int wert = 0;
 
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             wert += Zufall.zufallGanz(1, X);
         }
         return wert;
@@ -69,52 +69,68 @@ public class PenAndPaper {
             return wuerfel(N, X);
         }
         else {
-            return -1;
+            return 0;
         }
     }
 
     public static int angriff(int N, int X, int enemyAC, boolean vor, boolean nach, int atkB, int dmgB) {
         boolean treffer = check(enemyAC, vor, nach, atkB);
+
         if (treffer) {
             return wuerfel(N, X, dmgB);
         }
         else {
-            return -1;
+            return 0;
         }
     }
 
-    public static void meleeFight(Charakter A, Charakter B) {
-        Charakter a = A;
-        Charakter b = B;
-        int first = 0;
-        if (B.initiative < A.initiative) {
-            a = B;
-            b = A;
-            first = 1;
+    public static int meleeFight(Charakter A, Charakter B) {
+        Charakter[] charakters = new Charakter[2];
+        if (A.initiative < B.initiative) {
+            charakters[0] = A;
+            charakters[1] = B;
+        }
+        else {
+            charakters[0] = B;
+            charakters[1] = A;
         }
         int angriff = 0;
-        boolean advantage;
-        boolean disadvantage;
-        while (b.hitPoints > 0) {
-            if (b.roundsAdvantage > 0) {
-                advantage = true;
-                b.roundsAdvantage--;
-            }
-            if (b.roundsAdvantage > 0) {
-                disadvantage = true;
-                b.roundsDisadvantage--;
-            }
-            angriff = angriff(b.damageN, b.damageX, a.armorClass, );
-            a.hitPoints -= angriff;
-            if (a.hitPoints <= 0) {
-                break;
+        boolean advantage = false;
+        boolean disadvantage = false;
+        while (charakters[0].hitPoints > 0 && charakters[1].hitPoints > 0) {
+            for (int i = 0; i < charakters.length; i++) {
+                if (charakters[i].roundsAdvantage > 0) {
+                    advantage = true;
+                    charakters[i].roundsAdvantage--;
+                }
+                if (charakters[i].roundsAdvantage > 0) {
+                    disadvantage = true;
+                    charakters[i].roundsDisadvantage--;
+                }
+                angriff = angriff(charakters[i].damageN, charakters[i].damageX, charakters[Math.abs(i - 1)].armorClass, advantage, disadvantage, charakters[i].attackB, charakters[i].damageB);
+                charakters[Math.abs(i - 1)].hitPoints -= angriff;
+
+                if (charakters[Math.abs(i - 1)].hitPoints <= 0) {
+                    System.out.println(charakters[i].name + " hat gewonnen!");
+                    return i;
+                }
+                advantage = false;
+                disadvantage = false;
             }
         }
+        return 0;
     }
 
     public static void main(String[] args) {
-        Charakter Barbar = new Charakter("Barbar", 70, 14, 1, 1, 8, 0, 0, 3, 1);
-        Charakter Barde = new Charakter("Barde", 42, 10, 2, 1, 6, 0, 3, 0, 2);
-
+        int barde = 0;
+        for (int i = 0; i < 100; i++) {
+            Charakter Barbar = new Charakter("Barbar", 70, 14, 1, 1, 8, 0, 0, 3, 1);
+            Charakter Barde = new Charakter("Barde", 42, 18, 2, 1, 6, 0, 3, 0, 2);
+            barde += meleeFight(Barbar, Barde);
+        }
+        System.out.println("Barde: " + barde);
+        System.out.println("Barbar: " + (100 - barde));
     }
+
+    // Barde: Rüstungsklasse 10 -> 18
 }
